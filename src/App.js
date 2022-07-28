aa84dd7b61df18d473526b6da360d38fdb4a0e6d
Kenny <35975075+al1y3vk@users.noreply.github.com>
2022-07-28 02:53:18 -0700

    big

--- src/App.js
+++ src/App.js
@@ -1,60 +1,16 @@
+import React, { Component } from 'react';
+import SplitPane from './SplitPane'
 import './App.css';
 
-function FancyBorder(props) {
-  return (
-    <div className={'FancyBorder FancyBorder-' + props.color}>
-      {props.children}
-    </div>
-  );
-}
 
-function Dialog(props) {
-  return (
-    <FancyBorder color="blue">
-      <h1 className="Dialog-title">
-        {props.title}
-      </h1>
-      <p className="Dialog-message">
-        {props.message}
-      </p>
-    </FancyBorder>
-  );
-}
-
-function Contacts() {
-  return <div className="Contacts" />;
-}
-
-function Chat() {
-  return <div className="Chat" />;
-}
-
-function SplitPane(props) {
-  return (
-    <div className="SplitPane">
-      <div className="SplitPane-left">
-        {props.left}
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SplitPane />
       </div>
-      <div className="SplitPane-right">
-        {props.right}
-      </div>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <Dialog
-    title="Welcome"
-    message="Thank you for visiting our spacecraft!" />,
-    <SplitPane
-      left={
-        <Contacts />
-      }
-      right={
-        <Chat />
-      } />
-  );
+    );
+  }
 }
 
 export default App;
