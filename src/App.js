ecc1d0a839d3bb0c0ea1205159e6026be9ded2cb
Kenny <35975075+al1y3vk@users.noreply.github.com>
2022-07-28 03:39:29 -0700

    final

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Title from './Title'
 import IPinfo from './IPinfo'
 import Info from './Info'
+import NetData from './NetData';
 import './App.css';
 
 class App extends Component {
@@ -10,8 +11,16 @@ class App extends Component {
       <div>
         <Title titleText="Dashboard" />
 
-        <Info name="Public IPv6 Address">
+        <Info name="Public IPv4 Address">
           <IPinfo url='https://api.ipify.org?format=json' />
+        </Info>
+
+        <Info name="Public IPv6 Address">
+          <IPinfo url='https://api64.ipify.org?format=json' />
+        </Info>
+
+        <Info name="Server Latency">
+          <NetData />
         </Info>
       </div>
     );
