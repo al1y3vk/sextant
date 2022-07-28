14151e9e6e8e2caa00c8e7facb641c9f66623da7
Kenny <35975075+al1y3vk@users.noreply.github.com>
2022-07-28 01:58:30 -0700

    first major changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
 
+function Contacts() {
+  return <div className="Contacts" />;
+}
+
+function Chat() {
+  return <div className="Chat" />;
+}
+
+function SplitPane(props) {
+  return (
+    <div className="SplitPane">
+      <div className="SplitPane-left">
+        {props.left}
+      </div>
+      <div className="SplitPane-right">
+        {props.right}
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <SplitPane
+      left={
+        <Contacts />
+      }
+      right={
+        <Chat />
+      } />
   );
 }
 
